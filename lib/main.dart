42b54cd092a3c01da0ad8d1db59f9168904b5563
rameez <codewithrameez@gmail.com>
2021-05-25 02:21:36 +0530

    V3 Finished - Learnt adding images, inputs and ElevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'pages/home.dart';
 import 'pages/login.dart';
 
 void main() {
-  runApp(myApp());
+  runApp(MyApp());
 }
 
-class myApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.green),
+      theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          fontFamily: GoogleFonts.lato().fontFamily),
       darkTheme: ThemeData(brightness: Brightness.dark),
-      initialRoute: "/home",
+      initialRoute: "/",
       routes: {
         "/": (context) => Login(),
         "/home": (context) => Home(),
